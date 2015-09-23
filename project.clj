a1bb1cc216e62f36af77182264234fd8543b45dd
Andrey Antukh <niwi@niwi.nz>
2015-09-23 19:15:28 +0300

    Add missing project.clj

---
+++ project.clj
@@ -0,0 +1,18 @@
+(defproject funcool/beicon "0.1.0-SNAPSHOT"
+  :description "A rxjs idiomatic wrapper for ClojureScript"
+  :url "https://github.com/funcool/beicon"
+  :license {:name "BSD (2 Clause)"
+            :url "http://opensource.org/licenses/BSD-2-Clause"}
+  :dependencies [[org.clojure/clojure "1.7.0" :scope "provided"]
+                 [org.clojure/clojurescript "1.7.48" :scope "provided"]
+                 [funcool/cats "1.0.0"]]
+  :deploy-repositories {"releases" :clojars
+                        "snapshots" :clojars}
+  :source-paths ["src" "assets"]
+  :test-paths ["test"]
+  :jar-exclusions [#"\.swp|\.swo|user.clj"]
+  :codeina {:sources ["src"]
+            :reader :clojurescript
+            :target "doc/dist/latest/api"}
+  :plugins [[funcool/codeina "0.3.0"]
+            [lein-externs "0.1.3"]])
