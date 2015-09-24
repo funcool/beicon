bb0f0e03b055e90da0257bb514f40a223d45f51f
Andrey Antukh <niwi@niwi.nz>
2015-09-24 17:12:23 +0300

    Use nodejs repl on scripts/repl.clj instead of nashorn.
    Because rxjs does not works on nashorn.

--- scripts/repl.clj
+++ scripts/repl.clj
@@ -1,8 +1,9 @@
 (require
   '[cljs.repl :as repl]
-  '[cljs.repl.nashorn :as nashorn])
+  '[cljs.repl.nashorn :as nashorn]
+  '[cljs.repl.node :as node])
 
 (cljs.repl/repl
- (cljs.repl.nashorn/repl-env)
+ (cljs.repl.node/repl-env)
  :output-dir "out"
  :cache-analysis true)
