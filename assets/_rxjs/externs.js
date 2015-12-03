14777012ed3a3a5de8b8225a65cc9f276be425ba
Andrey Antukh <niwi@niwi.nz>
2015-12-03 21:51:06 +0200

    Add specific hand crafted externs with type annotations.

--- assets/_rxjs/externs.js
+++ assets/_rxjs/externs.js
@@ -1,47 +1,255 @@
-var TopLevel = {
-"amb" : function () {},
-"asObservable" : function () {},
-"bufferWithCount" : function () {},
-"clearInterval" : function () {},
-"concat" : function () {},
-"connect" : function () {},
-"ConnectableObservable" : function () {},
-"create" : function () {},
-"dispose" : function () {},
-"distinct" : function () {},
-"distinctUntilChanged" : function () {},
-"Error" : function () {},
-"filter" : function () {},
-"flatMap" : function () {},
-"fromArray" : function () {},
-"ignoreElements" : function () {},
-"just" : function () {},
-"map" : function () {},
-"merge" : function () {},
-"Observable" : function () {},
-"onCompleted" : function () {},
-"onError" : function () {},
-"onNext" : function () {},
-"pausable" : function () {},
-"pausableBuffered" : function () {},
-"publish" : function () {},
-"reduce" : function () {},
-"repeat" : function () {},
-"Rx" : function () {},
-"setInterval" : function () {},
-"skip" : function () {},
-"skipUntil" : function () {},
-"skipWhile" : function () {},
-"slice" : function () {},
-"Subject" : function () {},
-"subscribe" : function () {},
-"subscribeOnCompleted" : function () {},
-"subscribeOnError" : function () {},
-"subscribeOnNext" : function () {},
-"take" : function () {},
-"takeWhile" : function () {},
-"tap" : function () {},
-"throttle" : function () {},
-"timer" : function () {},
-"zip" : function () {}
-}
+/**
+ * @const
+ */
+var Rx = function() {};
+
+/**
+ * @constructor
+ */
+Rx.Observable = function() {};
+
+/**
+ * @constructor
+ * @extends {Rx.Observable}
+ */
+Rx.Subject = function() {};
+
+
+/**
+ * @constructor
+ * @extends {Rx.Observable}
+ */
+Rx.ConnectableObservable = function() {};
+
+/**
+ * @this {Rx.Subject}
+ * @return {undefined}
+ */
+Rx.Subject.prototype.onNext = function() {};
+
+/**
+ * @this {Rx.Subject}
+ * @return {undefined}
+ */
+Rx.Subject.prototype.onError = function() {};
+
+/**
+ * @this {Rx.Subject}
+ * @return {undefined}
+ */
+Rx.Subject.prototype.onCompleted = function() {};
+
+/**
+ * @constructor
+ */
+Rx.Disposable = function() {};
+
+/**
+ * @this {Rx.Disposable}
+ * @return {undefined}
+ */
+Rx.Disposable.prototype.dispose = function() {};
+
+/**
+ * @this {null}
+ * @return {Rx.Observable}
+ */
+Rx.Observable.create = function() {};
+
+/**
+ * @this {null}
+ * @return {Rx.Observable}
+ */
+Rx.Observable.repeat = function() {};
+
+/**
+ * @this {null}
+ * @return {Rx.Observable}
+ */
+Rx.Observable.fromArray = function() {};
+
+/**
+ * @this {null}
+ * @return {Rx.Observable}
+ */
+Rx.Observable.just = function() {};
+
+/**
+ * @this {null}
+ * @return {Rx.Observable}
+ */
+Rx.Observable.timer = function() {};
+
+/**
+ * @this {Rx.Observable}
+ * @return {Rx.Disposable}
+ */
+Rx.Observable.prototype.subscribe = function() {};
+
+/**
+ * @this {Rx.Observable}
+ * @return {Rx.Disposable}
+ */
+Rx.Observable.prototype.subscribeOnNext = function() {};
+
+/**
+ * @this {Rx.Observable}
+ * @return {Rx.Disposable}
+ */
+Rx.Observable.prototype.subscribeOnError = function() {};
+
+/**
+ * @this {Rx.Observable}
+ * @return {Rx.Disposable}
+ */
+Rx.Observable.prototype.subscribeOnCompleted = function() {};
+
+/**
+ * @this {Rx.Observable}
+ * @return {Rx.Observable}
+ */
+Rx.Observable.prototype.amb = function() {};
+
+/**
+ * @this {Rx.Observable}
+ * @return {Rx.Observable}
+ */
+Rx.Observable.prototype.zip = function() {};
+
+/**
+ * @this {Rx.Observable}
+ * @return {Rx.Observable}
+ */
+Rx.Observable.prototype.publish = function() {};
+
+/**
+ * @this {Rx.Observable}
+ * @return {Rx.Observable}
+ */
+Rx.Observable.prototype.connect = function() {};
+
+/**
+ * @this {Rx.Observable}
+ * @return {Rx.Observable}
+ */
+Rx.Observable.prototype.concat = function() {};
+
+/**
+ * @this {Rx.Observable}
+ * @return {Rx.Observable}
+ */
+Rx.Observable.prototype.merge = function() {};
+
+/**
+ * @this {Rx.Observable}
+ * @return {Rx.Observable}
+ */
+Rx.Observable.prototype.filter = function() {};
+
+/**
+ * @this {Rx.Observable}
+ * @return {Rx.Observable}
+ */
+Rx.Observable.prototype.map = function() {};
+
+/**
+ * @this {Rx.Observable}
+ * @return {Rx.Observable}
+ */
+Rx.Observable.prototype.flatMap = function() {};
+
+/**
+ * @this {Rx.Observable}
+ * @return {Rx.Observable}
+ */
+Rx.Observable.prototype.skip = function() {};
+
+/**
+ * @this {Rx.Observable}
+ * @return {Rx.Observable}
+ */
+Rx.Observable.prototype.skipWhile = function() {};
+
+/**
+ * @this {Rx.Observable}
+ * @return {Rx.Observable}
+ */
+Rx.Observable.prototype.skipUntil = function() {};
+
+/**
+ * @this {Rx.Observable}
+ * @return {Rx.Observable}
+ */
+Rx.Observable.prototype.take = function() {};
+
+/**
+ * @this {Rx.Observable}
+ * @return {Rx.Observable}
+ */
+Rx.Observable.prototype.takeWhile = function() {};
+
+/**
+ * @this {Rx.Observable}
+ * @return {Rx.Observable}
+ */
+Rx.Observable.prototype.slice = function() {};
+
+/**
+ * @this {Rx.Observable}
+ * @return {Rx.Observable}
+ */
+Rx.Observable.prototype.reduce = function() {};
+
+/**
+ * @this {Rx.Observable}
+ * @return {Rx.Observable}
+ */
+Rx.Observable.prototype.tap = function() {};
+
+/**
+ * @this {Rx.Observable}
+ * @return {Rx.Observable}
+ */
+Rx.Observable.prototype.throttle = function() {};
+
+/**
+ * @this {Rx.Observable}
+ * @return {Rx.Observable}
+ */
+Rx.Observable.prototype.pausableBuffered = function() {};
+
+/**
+ * @this {Rx.Observable}
+ * @return {Rx.Observable}
+ */
+Rx.Observable.prototype.pausable = function() {};
+
+/**
+ * @this {Rx.Observable}
+ * @return {Rx.Observable}
+ */
+Rx.Observable.prototype.distinctUntilChanged = function() {};
+
+/**
+ * @this {Rx.Observable}
+ * @return {Rx.Observable}
+ */
+Rx.Observable.prototype.distinct = function() {};
+
+/**
+ * @this {Rx.Observable}
+ * @return {Rx.Observable}
+ */
+Rx.Observable.prototype.bufferWithCount = function() {};
+
+/**
+ * @this {Rx.Observable}
+ * @return {Rx.Observable}
+ */
+Rx.Observable.prototype.asObservable = function() {};
+
+/**
+ * @this {Rx.Observable}
+ * @return {Rx.Observable}
+ */
+Rx.Observable.prototype.distinct = function() {};
