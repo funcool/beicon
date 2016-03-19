4d396a6b98fabe63dc1df8b95d1e2bcadb81d842
Andrey Antukh <niwi@niwi.nz>
2016-03-19 10:42:55 +0200

    Rename Rx.umd.* rx.*

--- assets/deps.cljs
+++ assets/deps.cljs
@@ -1,6 +1,5 @@
 {:foreign-libs
-
- [{:file "_rxjs/Rx.umd.js"
-   :file-min "_rxjs/Rx.umd.min.js"
+ [{:file "_rxjs/rx.js"
+   :file-min "_rxjs/rx.min.js"
    :provides ["beicon.extern.rxjs"]}]
  :externs ["_rxjs/externs.js"]}
