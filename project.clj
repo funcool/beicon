9a26f9ab186db800d842ee412cd74371f1168162
Andrey Antukh <niwi@niwi.nz>
2016-03-19 10:45:07 +0200

    Update dependencies.

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "https://github.com/funcool/beicon"
   :license {:name "Public Domain" :url "http://unlicense.org/"}
   :dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]
-                 [org.clojure/clojurescript "1.7.228" :scope "provided"]
-                 [funcool/promesa "0.8.1" :scope "provided"]]
+                 [org.clojure/clojurescript "1.8.34" :scope "provided"]
+                 [funcool/promesa "1.1.1" :scope "provided"]]
   :deploy-repositories {"releases" :clojars
                         "snapshots" :clojars}
   :source-paths ["src" "assets"]
