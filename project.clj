10e99559dd2be2fbf57c243abb51537db98c32d2
Andrey Antukh <niwi@niwi.nz>
2017-11-17 10:44:01 +0100

    Update dependencies.

--- project.clj
+++ project.clj
@@ -5,8 +5,8 @@
             :url "http://opensource.org/licenses/BSD-2-Clause"}
 
   :dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]
-                 [org.clojure/clojurescript "1.9.854" :scope "provided"]
-                 [io.reactivex.rxjava2/rxjava "2.1.2"]]
+                 [org.clojure/clojurescript "1.9.946" :scope "provided"]
+                 [io.reactivex.rxjava2/rxjava "2.1.6"]]
 
   :deploy-repositories {"releases" :clojars
                         "snapshots" :clojars}
@@ -22,11 +22,11 @@
 
   :profiles
   {:dev {:dependencies [[org.clojure/tools.namespace "0.2.11"]
-                        [funcool/promesa "1.8.1"]]
+                        [funcool/promesa "1.9.0"]]
          :aliases {"test-all" ["with-profile" "dev,1.9:dev,1.7:dev" "test"]}
          :global-vars {*warn-on-reflection* false}
          :plugins [[funcool/codeina "0.5.0"]
                    [lein-ancient "0.6.10"]]}
-   :1.9 {:dependencies [[org.clojure/clojure "1.9.0-alpha16"]]}
+   :1.9 {:dependencies [[org.clojure/clojure "1.9.0-rc1"]]}
    :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}})
 
